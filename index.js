a12e540b49d29b2355c945fb2edd7df5607b929a
Shrutiiiz <suranishruti5499@gmail.com>
2025-07-14 10:56:17 +0530

    practice of program

---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log("hello world!!! ")
+
+// 1. Check Even or Odd
+const num = 8;
+if (num % 2 == 0) 
+{
+    console.log("Even");
+} else 
+{
+    console.log("Odd");
+}
+        
+// 2. Find the Maximum of Two Numbers
+const num1 = 8;
+const num2 = 8;
+if (num % 2 == 0) 
+{
+    console.log("Even");
+} else 
+{
+    console.log("Odd");
+}
