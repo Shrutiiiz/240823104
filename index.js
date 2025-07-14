957969cbe526bc8407f212159cec3c2ce356ca0b
Shrutiiiz <suranishruti5499@gmail.com>
2025-07-14 11:44:50 +0530

    today work

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 // console.log("hello world!!! ")
 
 // 1. Check Even or Odd
-const num = 8;
-if (num % 2 == 0) 
-{
-    console.log("Even");
-} else 
-{
-    console.log("Odd");
-}
+// const num = 8;
+// if (num % 2 == 0) 
+// {
+//     console.log("Even");
+// } else 
+// {
+//     console.log("Odd");
+// }
         
 // 2. Find the Maximum of Two Numbers
-const num1 = 8;
-const num2 = 8;
-if (num % 2 == 0) 
-{
-    console.log("Even");
-} else 
-{
-    console.log("Odd");
-}
-console.log("complete");
+// const a = 8;
+// const b = 9;
+// if (a<b) 
+// {
+//     console.log(b,"is max");
+// } else 
+// {
+//     console.log(a,"is max");
+// }
+
+// 3. Check Leap Year
+// const year = 2020;
+// if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+// {
+//     console.log("Leap Year");
+// } 
+// else 
+// {
+//     console.log("Not a Leap Year");
+// }
+
+// 4. Sum of Natural Numbers
+// const n = 5;
+// const sum = (n * (n + 1)) / 2;
+// console.log("Sum is:", sum);
+
+// 5. Factorial of a Number
+// const number = 4;
+// const factorial = 1;
+// for (let i = 1; i <= number; i++) {
+//     factorial *= i;
+// }
+// console.log("Factorial is:", factorial); 
