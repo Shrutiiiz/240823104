4d2039634fab4c350f78b04ab63bbf4f08a135e9
Shrutiiiz <suranishruti5499@gmail.com>
2025-07-21 11:35:35 +0530

    programs

--- index.js
+++ index.js
@@ -53,9 +53,21 @@
 // }
 
 // 7. Reverse a Number
+// with function
 // const n = 123456;
 // const rn = n.toString().split("").reverse().join("");
 // console.log("Reversed Number:", rn);
+
+// without function
+// var n=1234
+// var rem,rev = 0
+// while(n!=0)
+// {
+//     rem = n%10
+//     rev = rev*10+rem
+//     n = Math.floor(n/10)
+// }
+// console.log(rev)
 
 // 8. Palindrome Check (Number)
 // const n = 12321;
@@ -86,3 +98,77 @@
 // {
 //     console.log("It's Not Prime Number");
 // }
+
+// 10. Count Digits in a Number
+// let n = 502345;
+// let c = n.toString().length;
+// console.log("Number of digits:", c);
+
+// 11. Sum of Digits
+// let n = 1234;
+// let digits = n.toString().split('');
+// let sum = 0;
+// for (let digit of digits)
+// {
+//   sum += parseInt(digit);
+// }
+// console.log("Sum of digits:", sum);
+
+// 12. Check Armstrong Number --------------------------------------gpt
+// let number = 153;
+// let digits = number.toString().split('');
+// let power = digits.length;
+
+// let sum = 0;
+// for (let digit of digits) {
+//   sum += Math.pow(parseInt(digit), power);
+// }
+
+// if (sum === number) {
+//   console.log("Armstrong");
+// } else {
+//   console.log("Not Armstrong");
+// }
+
+// 13. Generate Fibonacci Series
+// let n = 8
+// let a = 0
+// let b = 1
+// console.log("Fibonacci Series-> ");
+// for (let i = 0; i < n; i++) 
+// {
+//   console.log(a);
+//   let next = a + b;
+//   a = b;
+//   b = next;
+// }
+
+// 14. Check Vowel or Consonant
+// const char = 'a';
+
+// if ('aeiouAEIOU'.includes(char)) {
+//   console.log("Vowel");
+// } else {
+//   console.log("Consonant");
+// }
+
+// 15. Simple Calculator
+// const num1 = 4;
+// const num2 = 2;
+// const operator = '+';
+
+// let result;
+
+// if (operator === '+') {
+//   result = num1 + num2;
+// } else if (operator === '-') {
+//   result = num1 - num2;
+// } else if (operator === '*') {
+//   result = num1 * num2;
+// } else if (operator === '/') {
+//   result = num1 / num2;
+// } else {
+//   console.log("Invalid operator");
+// }
+
+// console.log("Result:", result);
